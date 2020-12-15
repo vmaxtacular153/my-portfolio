e1f77fb96c4f4bd93ffe4c58ceb18c9f646481ff
vmaxtacular513 <veronicamaxwell152@gmail.com>
2020-12-15 10:19:39 -0500

    Added styling to portfolio-items

--- src/App.js
+++ src/App.js
@@ -81,18 +81,23 @@ function App() {
       {/* My Work section */}
       <section className='my-work' id='work'>
         <h2 className='section-title title-work'>My Work</h2>
-        <p className='section-subtitle subtitle-work'></p>
+        <p className='section-subtitle subtitle-work'>A selection of my range of work</p>
         <div className='portfolio'>
           <a href='' className='portfolio-item'>
             <img src={Portfolio} alt='' className='portfolio-img' />
           </a>
-        </div>
-        <div className='portfolio'>
           <a href='' className='portfolio-item'>
             <img src={Portfolio1} alt='' className='portfolio-img' />
           </a>
-        </div>
-        <div className='portfolio'>
+          <a href='' className='portfolio-item'>
+            <img src={Portfolio2} alt='' className='portfolio-img' />
+          </a>
+          <a href='' className='portfolio-item'>
+            <img src={Portfolio} alt='' className='portfolio-img' />
+          </a>
+          <a href='' className='portfolio-item'>
+            <img src={Portfolio1} alt='' className='portfolio-img' />
+          </a>
           <a href='' className='portfolio-item'>
             <img src={Portfolio2} alt='' className='portfolio-img' />
           </a>
