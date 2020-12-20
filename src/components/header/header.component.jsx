fc459bff07c30fa616eb335be6a5f07841e867ee
vmaxtacular513 <veronicamaxwell152@gmail.com>
2020-12-20 14:43:36 -0500

    refractored header component to add bootstrap

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,20 +6,8 @@ const Header = () => {
     return (
         <div>
             <header>
-                <div className='logo'>
-                <img src={logo} alt='logo' />
-                </div>
-                <button className='nav-toggle' aria-label='toggle-navigation'>
-                <span className='hamburger'></span>
-                </button>
-                <nav className='nav'>
-                <ul className='nav-list'>
-                    <li className='nav-item nav-link'><a href='#'>Home</a></li>
-                    <li className='nav-item nav-link'><a href='#'>My Services</a></li>
-                    <li className='nav-item nav-link'><a href='#'>My Work</a></li>
-                    <li className='nav-item nav-link'><a href='#'>Contact</a></li>
-                </ul>
-                </nav>
+                <img src={logo} alt='logo' className='logo' />
+                <nav className='navigation'></nav>
             </header>
         </div>
     )
