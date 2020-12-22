516629b0993b50556b75715e5eb063fc3d4b7b68
vmaxtacular513 <veronicamaxwell152@gmail.com>
2020-12-22 16:09:15 -0500

    Added styling and refactored my-work and portfolio-item components

--- src/components/my-work/my-work.component.jsx
+++ src/components/my-work/my-work.component.jsx
@@ -1,44 +1,19 @@
 import React from 'react';
-import Portfolio from '../../assets/portfolio.jpg';
-import Portfolio1 from '../../assets/portfolio1.jpg';
-import Portfolio2 from '../../assets/portfolio2.jpg';
+import Portfolio from '../portfolio/portfolio.component';
 import './my-work.style.css';
 
 
 const MyWork = () => {
     return (
-        <div>
             <section className='my-work' id='work'>
+                <div>
                 <h2 className='section-title title-work'>My Work</h2>
                 <p className='section-subtitle subtitle-work'>A selection of my range of work</p>
-                <div className='portfolio'>
-                <a href='' className='portfolio-item'>
-                    <img src={Portfolio} alt='' className='portfolio-img' />
-                </a>
-                <a href='' className='portfolio-item'>
-                    <img src={Portfolio1} alt='' className='portfolio-img' />
-                </a>
-                <a href='' className='portfolio-item'>
-                    <img src={Portfolio2} alt='' className='portfolio-img' />
-                </a>
-                <a href='' className='portfolio-item'>
-                    <img src={Portfolio} alt='' className='portfolio-img' />
-                </a>
-                <a href='' className='portfolio-item'>
-                    <img src={Portfolio1} alt='' className='portfolio-img' />
-                </a>
-                <a href='' className='portfolio-item'>
-                    <img src={Portfolio2} alt='' className='portfolio-img' />
-                </a>
-                <a href='' className='portfolio-item'>
-                    <img src={Portfolio} alt='' className='portfolio-img' />
-                </a>
-                <a href='' className='portfolio-item'>
-                    <img src={Portfolio2} alt='' className='portfolio-img' />
-                </a>
+                </div>
+                <div className='portfolio-grid'>
+                    <Portfolio />
                 </div>
             </section>
-        </div>
     )
 }
 
